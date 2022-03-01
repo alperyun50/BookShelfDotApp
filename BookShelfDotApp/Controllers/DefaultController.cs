2d197e3a8b2bb85c45bc39e7c4119d0bc1163981
alper yavuz ün <yavuzun50@hotmail.com>
2022-03-01 19:48:16 +0300

    mvc folders and wwwroot added

---
+++ BookShelfDotApp/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using BookShelfDotApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShelfDotApp.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            var bk = new List<Books>()
+                {
+                   new Books() {Id=1, BookName="Pürdikkat", Author="Cal Newport" },
+                   new Books() {Id=2, BookName="Cesur yeni dünya", Author="Aldous Huxley"},
+                   new Books() {Id=3, BookName="Ben robot", Author="Isaac Asimov"},
+                   new Books() {Id=4, BookName="Homo ludens", Author="Johan Huizinga"},
+                   new Books() {Id=5, BookName="Sistem", Author="James Ball"}
+                };
+            
+            return View(bk);
+        }
+    }
+}
---
+++ BookShelfDotApp/ViewComponents/NewBooks.cs
@@ -0,0 +1,23 @@
+﻿using BookShelfDotApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShelfDotApp.ViewComponents
+{
+    public class NewBooks : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var bookList = new List<Books>()
+            {
+                new Books() {Id=6, BookName="Korku", Author="Zweig"},
+                new Books() {Id=7, BookName="Anna Karanina", Author="Tolstoy"}
+            };
+
+            return View(bookList);
+        }
+    }
+}
